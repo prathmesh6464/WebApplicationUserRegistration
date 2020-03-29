1ae97a19de90d44d9f93c8becbe0f0ef9f1e7345
Prathamesh <prathmesh6464@gmail.com>
2020-03-29 21:03:22 +0530

    REFACTORED : Added mql data base remove oracle database.

--- src/com/bridz/insert_into_database/InsertDataBase.java
+++ src/com/bridz/insert_into_database/InsertDataBase.java
@@ -5,44 +5,45 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 
-//INTERFACE ORACLE HELPER
-interface OracleHelper
-{
-	void getOracleDbConnection() throws Exception;
+//INTERFACE MY SQL HELPER
+interface MySqlHelper {
+	void getMySqlDbConnection() throws Exception;
 }
 
 
-//CLASS CONNECTION DATABASE ACTS AS A FACADE
-class ConnectionDatabase implements OracleHelper
-{
-	public void getOracleDbConnection() throws Exception
-	{
-		String url = "jdbc:oracle:thin:@localhost:1521:xe";
-		String userName = "system";
+//CLASS CONNECTION DATABASE BY USING MYSQL
+class ConnectionDatabase implements MySqlHelper {
+	
+	public void getMySqlDbConnection() throws Exception	{
+		
+		String url = "jdbc:mysql://localhost:3306/TestDataBase";
+		String userName = "root";
 		String password = "admin";
-		String query = "select * from customers";
+		String query = "select * from student";
 
-		Class.forName("oracle.jdbc.driver.OracleDriver");
-		Connection connectionWithOracle = DriverManager.getConnection(url, userName, password);
-		Statement statement = connectionWithOracle.createStatement();
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection connectionWithMysql = DriverManager.getConnection(url, userName, password);
+		Statement statement = connectionWithMysql.createStatement();
 		ResultSet resultSet = statement.executeQuery(query);
 
-		while(resultSet.next())
-		{
-			int customerID = resultSet.getInt("customer_id");
-			String customerName = resultSet.getString("customer_name");
-			String customerCity = resultSet.getString("city");
-			System.out.println("Cutomer id : "+customerID);
-			System.out.println("Cutomer name : "+customerName);
-			System.out.println("Cutomer city : "+customerCity);
+		while(resultSet.next()) {
+			int studentRollNumber = resultSet.getInt(1);
+			String studentName = resultSet.getString(2);
+			System.out.println("Student roll number : "+studentRollNumber);
+			System.out.println("Student name : "+studentName);
 		}
 		resultSet.close();
-		connectionWithOracle.close();
+		connectionWithMysql.close();
 	}
 }
 
 
 public class InsertDataBase {
 	
-
+	//RUN METHOD
+	public static void run() throws Exception {
+		
+	ConnectionDatabase connectionDatabaseObject =  new ConnectionDatabase();
+	connectionDatabaseObject.getMySqlDbConnection();
+	}
 }
