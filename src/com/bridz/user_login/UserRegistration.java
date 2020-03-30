7f4e932b7a7df47adb90ca09127cd91845c08357
Prathamesh <prathmesh6464@gmail.com>
2020-03-30 18:41:34 +0530

    MODIFIED : Changed indentation.

--- src/com/bridz/user_login/UserRegistration.java
+++ src/com/bridz/user_login/UserRegistration.java
@@ -1,8 +1,6 @@
 package com.bridz.user_login;
 import com.bridz.insert_into_database.*;
-
 import java.lang.ProcessBuilder.Redirect;
-
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -25,11 +23,10 @@ public class UserRegistration extends HttpServlet {
 		System.out.println("User name : "+userName);
 		System.out.println("Password : "+password);
 		try {
-			
 			InsertDataBase.run(nameOfUser, lastNameOfUser, userName, password);
 			response.sendRedirect("http://localhost:8081/WebApplicationUserRegistration/Welcome.jsp");
 		} catch (Exception e) {
-		
+
 			e.printStackTrace();
 		}
 	}
