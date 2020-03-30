f04d659a7b8b0fb12129c1c7315c891d2335eb93
Prathamesh <prathmesh6464@gmail.com>
2020-03-30 17:24:37 +0530

    MODIFIED : Added add insert database feature.

--- src/com/bridz/insert_into_database/InsertDataBase.java
+++ src/com/bridz/insert_into_database/InsertDataBase.java
@@ -6,28 +6,29 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 
-
-
 //CLASS CONNECTION DATABASE BY USING MYSQL
 public class InsertDataBase {
-
-	public void getMySqlDbConnection(String firstNameToStore, String lastNameToStore, String userNameToStore, String passwordToStore) throws Exception {
-
+	
+	public void getMySqlDbConnection(String firstNameToStore, String lastNameToStore, String userNameToStore, String passwordToStore) throws Exception
+	{
 		String url = "jdbc:mysql://localhost:3306/UserDataBase";
 		String userName = "root";
 		String password = "admin";
-		String query = "select into user_form (first_name,last_name,user_name,password) values ('prathamesh','jadhav','prathm62@gmail.com','123#1`2')";
-
+		//String query = "select * from student";
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		Connection connectionWithMysql = DriverManager.getConnection(url, userName, password);
-		Statement statement = connectionWithMysql.createStatement();
-		statement.executeUpdate(query);
-		PreparedStatement stmt = connectionWithMysql.prepareStatement("insert into user_form values(?,?,?,?)");  
-		stmt.setInt(1,101);//1 specifies the first parameter in the query  
-		stmt.setString(2,"Ratan");  
-		
+		PreparedStatement preparedStatement = connectionWithMysql.prepareStatement("insert into user_form values(?,?,?,?)");
+
+		preparedStatement.setString(1,firstNameToStore);//1 specifies the first parameter in the query  
+		preparedStatement.setString(2,lastNameToStore);
+		preparedStatement.setString(3,userNameToStore);
+		preparedStatement.setString(4,passwordToStore);
+		preparedStatement.executeUpdate();
+		preparedStatement.close();
+		  
 /*
-		while(resultSet.next()) {
+		while(resultSet.next())
+		{
 			int studentRollNumber = resultSet.getInt(1);
 			String studentName = resultSet.getString(2);
 			System.out.println("Student roll number : "+studentRollNumber);
@@ -36,23 +37,12 @@ public class InsertDataBase {
 		resultSet.close();*/
 		connectionWithMysql.close();
 	}
+
 	
 	public static void run(String firstNameToStore, String lastNameToStore, String userNameToStore , String passwordToStore) throws Exception {
 
 		//CREATED OBJECT OF CONNECTION DATABASE
 		InsertDataBase connectionDatabaseObject =  new InsertDataBase();
-		connectionDatabaseObject.getMySqlDbConnection(firstNameToStore, lastNameToStore, userNameToStore, passwordToStore);
+		connectionDatabaseObject.getMySqlDbConnection(firstNameToStore, lastNameToStore, userNameToStore,passwordToStore);
 	}
 }
-
-/*
-class InsertDataBase {
-	
-	//RUN METHOD
-	public static void main(String args[]) throws Exception {
-
-		//CREATED OBJECT OF CONNECTION DATABASE
-		InsertDatabase connectionDatabaseObject =  new InsertDatabase();
-		connectionDatabaseObject.getMySqlDbConnection();
-	}
-}*/
