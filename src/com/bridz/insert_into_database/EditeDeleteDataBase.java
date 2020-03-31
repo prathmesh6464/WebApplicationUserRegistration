1af8c45af5ec9a84a26a6a397b49790eca78540e
Prathamesh <prathmesh6464@gmail.com>
2020-04-01 01:08:31 +0530

    MODIFIED : Added send redirect feature for edite.

--- src/com/bridz/insert_into_database/EditeDeleteDataBase.java
+++ src/com/bridz/insert_into_database/EditeDeleteDataBase.java
@@ -2,26 +2,29 @@ package com.bridz.insert_into_database;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.sql.Statement;
-
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import com.bridz.insert_into_database.*;
 
-public class EditeDeleteDataBase extends HttpServlet {
 
+public class EditeDeleteDataBase extends HttpServlet {
+	
 	//VARIABLE
 	static final long serialVersionUID = -156154208445344184L;
+	public static String idOption = "";
+	
+	public  void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-	public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
-		String idOption = request.getParameter("id");
-		String buttonOption = request.getParameter("button");
+		idOption = request.getParameter("id");
+		String buttonOption = request.getParameter("button");			
 		System.out.println(idOption);
 		System.out.println(buttonOption);
 
 		if(buttonOption.equals("Delete")) {
+			
 			String query = "delete from user_form where user_id = "+idOption;
+			
 			try {
 				Statement statement = InsertDataBase.getConection().createStatement();
 				statement.executeUpdate(query);
@@ -31,7 +34,11 @@ public class EditeDeleteDataBase extends HttpServlet {
 				e.printStackTrace();
 			}
 		}
-		//if(buttonOption2.equals("Edite")) {
-		//}
+	
+		if(buttonOption.equals("Edite")) {			
+			
+			response.sendRedirect("http://localhost:8081/WebApplicationUserRegistration/EditeData.jsp");			
+		}
 	}
 }
+
